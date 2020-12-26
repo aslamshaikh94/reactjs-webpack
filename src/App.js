de39758b825c982c9cf939bf5d2e93889999dc07
Aslam Ahmed <aslamahmed@Aslams-MacBook-Pro.local>
2020-12-26 17:53:18 +0530

    Add all

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import './style.scss'
+
+const App = () => {
+  return (
+    <div>
+      <h1>Hello</h1>
+      <p>This is React.js simple setup with Webpack and SASS</p>
+    </div>
+  )
+}
+
+export default App
---
+++ webpack.config.js
@@ -0,0 +1,33 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const path = require('path')
+
+module.exports = {
+  module: {
+    rules: [
+      {
+        test: /\.scss$/,
+        use: [
+          {
+            loader: 'style-loader',
+          },
+          {
+            loader: 'css-loader',
+          },
+          {
+            loader: 'sass-loader',
+          },
+        ],
+      },
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: ['babel-loader'],
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: path.resolve(__dirname, 'src', 'index.html'),
+    }),
+  ],
+}
